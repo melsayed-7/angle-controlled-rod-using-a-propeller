ad069b05ae3b2ebd98524789fdb1a2c28c546670
moustafa-7 <moustafasleem777@gmail.com>
2019-04-23 00:08:49 +0200

    must commit to pull

--- final project/modeling.m
+++ final project/modeling.m
@@ -4,10 +4,10 @@ close all
 a1=csvread('4.csv');
 b1=csvread('5.csv');
 figure 
-plot(a(:,1));
+plot(a1(:,1));
 figure
-plot(a(:,2));
+plot(a1(:,2));
 
-plot(b(:,1));
+plot(b1(:,1));
 figure
-plot(b(:,2));
+plot(b1(:,2));
