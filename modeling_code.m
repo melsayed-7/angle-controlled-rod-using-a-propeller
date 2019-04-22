e0e9d6eb5b7cae47dd8cb10a4f68e8892216d390
moustafa-7 <moustafasleem777@gmail.com>
2019-04-22 15:50:06 +0200

    data
    Check data_3_39 and data_3_48

--- modeling_code.m
+++ modeling_code.m
@@ -2,12 +2,14 @@
 plot(t,data);
 ts = 17/500;  % this is the sampling time it is computed from the array t
 
-data1 = iddata(data',[zeros(1,floor(4/ts)) ones(1,(557-floor(4/ts)))]',.034);  % this is processing the data to make it compatible with the transfer-function estimation funciton
+data1 = iddata(data(1:543)',input_2',.034);  % this is processing the data to make it compatible with the transfer-function estimation funciton
 
-g = tfest(data1,5,2);   % getting the transfer function and storing it in g
+g = tfest(data1,2,0);   % getting the transfer function and storing it in g
   
 
 % We found that the best approximation to the system is a system of the
 % fifth order
+transfer_fn = tf([g.Numerator],[g.Denominator])
+x = step(transfer_fn)
+plot(x)
 
-    
